acd1d82786b458f5ef863bc0853cec320ca5d00d
kai <carlgammasagan@gmail.com>
2017-08-19 13:36:05 -0700

    inception commit

---
+++ GraphicsProgrammingEssay_v1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+printf("Hello Yo!\n");
+
+struct entertainment{
+	char*exampleMathematicalSubset;
+};
+struct entertainment Nagato={"graphics programming is usually a mathematical subset of the entertainment C-style struct"};
+struct practicalWork{
+	char*exampleMathematicalSubset;
+};
+struct practicalWork uk={"software engineering and being a general-purpose digital Jedi (minus things that are not practical-work-related and are also members of the software engineering and being a general-purpose digital Jedi superSets) are subsets of the practicalWork C-style struct"};
+
+printf("Uk says this to Nagato: \"%s.\"\n", Nagato.exampleMathematicalSubset);
+printf("Uk says this to himself: \"%s.\"\n", uk.exampleMathematicalSubset);
+printf("Uk says this to himself: \"Being fluent and fluid with C-style structs seems (\"being seems\"...correct grammar but awkward sounding in context, it seems...standard English grammar really arguably suxors sometimes. The ellipse isn't even sanctioned and it's such a natural&intuitive punctuation technique. Fuck standard shit sometimes...) necessary for being a legit C programmer, which is frosty miles to go before sleeping well as a 1337 C programmer and general-purpose digital Jedi.\"");
+return 0;
+}
